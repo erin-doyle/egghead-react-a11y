b69457ffa6c59cb00f57adcb4fc3111bf80c57cb
Erin Doyle <edoyle@cashstar.com>
2019-10-20 12:42:12 -0400

    Added form validation to the Login form showing errors for Username and Password fields when they're not populated upon form submit

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,34 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 
-const Login = ({ history }) => {
-    const goToWishlist = () => history.push('/wishlist');
+class Login extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="login row align-items-center">
-           <div className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5">
-                <div className="card bg-primary">
-                    <h5 className="card-header">
-                        Movie Wishlist Login
-                    </h5>
-                    <div className="card-body">
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input type="text" name="username" className="form-control" placeholder="Enter username" />
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" name="password" className="form-control" placeholder="Password" />
-                            <small className="form-text text-muted">Passwords are case sensitive</small>
-                        </div>
-                        <button type="button" className="btn btn-primary" onClick={goToWishlist}>Login</button>
+        this.state = {
+            username: '',
+            password: '',
+            isUsernameValid: true,
+            isPasswordValid: true
+        };
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    validateForm() {
+        const { username, password } = this.state;
+
+        const isUsernameValid = !!username;
+        const isPasswordValid = !!password;
+
+        this.setState({
+            isUsernameValid,
+            isPasswordValid
+        });
+
+        return isUsernameValid && isPasswordValid;
+    }
+
+    onSubmit() {
+        const { history } = this.props;
+
+        const isFormValid = this.validateForm();
+
+        if (isFormValid) {
+            history.push('/wishlist');
+        }
+    }
+
+    render() {
+        const { isUsernameValid, isPasswordValid } = this.state;
+        const usernameClasses = `form-control ${!isUsernameValid ? 'is-invalid' : ''}`;
+        const passwordClasses = `form-control ${!isPasswordValid ? 'is-invalid' : ''}`;
+
+        return (
+            <div className="login row align-items-center">
+                <div className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5">
+                    <div className="card bg-primary">
+                        <h5 className="card-header">
+                            Movie Wishlist Login
+                        </h5>
+                        <form>
+                            <div className="card-body">
+
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        className={usernameClasses}
+                                        placeholder="Enter username"
+                                        onChange={this.handleUsernameChange}
+                                    />
+                                    <div className="invalid-feedback">
+                                        Please provide a Username.
+                                    </div>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        className={passwordClasses}
+                                        placeholder="Password"
+                                        onChange={this.handlePasswordChange}
+                                    />
+                                    <small className="form-text text-muted">
+                                        Passwords are case sensitive
+                                    </small>
+                                    <div className="invalid-feedback">
+                                        Please provide a Password.
+                                    </div>
+                                </div>
+
+                                <button type="button" className="btn btn-primary" onClick={this.onSubmit}>Login</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 Login.propTypes = {
     history: PropTypes.object.isRequired
