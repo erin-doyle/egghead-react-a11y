840c130322779279c05696da70853b515174b7af
Erin Doyle <edoyle@cashstar.com>
2019-11-09 13:20:13 -0500

    Added tabIndex and focus management based on selected MovieToolbarButton in MovieToolbar

--- src/primitives/MovieToolbar.js
+++ src/primitives/MovieToolbar.js
@@ -1,28 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import MovieToolbarButton from './MovieToolbarButton';
 
 
-const MovieToolbar = ({ ariaLabel, movieTitle, buttonList }) => {
-    const buttonItems = buttonList.map((buttonItem) => {
-        const { title } = buttonItem;
+class MovieToolbar extends Component {
+    constructor(props) {
+        super(props);
+
+        const { buttonList } = this.props;
+
+        this.state = {
+            selectedButton: buttonList[0]
+        };
+
+        this.selectedButtonRef = null;
+
+        this.setSelectedButtonRef = this.setSelectedButtonRef.bind(this);
+        this.selectButton = this.selectButton.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { selectedButton } = this.state;
+
+        // Only when the selected button has changed do we need to focus that button
+        if (prevState.selectedButton !== selectedButton) {
+            if (this.selectedButtonRef) {
+                this.selectedButtonRef.focus();
+            }
+        }
+    }
+
+    setSelectedButtonRef(element) {
+        this.selectedButtonRef = element;
+    }
+
+    selectButton (button) {
+        this.setState({selectedButton: button});
+    }
+
+    handleClick (e, button) {
+        e.preventDefault();
+        this.selectButton(button);
+
+        // Fire the button's action
+        button.action();
+    }
+
+    render() {
+        const { ariaLabel, movieTitle, buttonList } = this.props;
+        const { selectedButton } = this.state;
+
+        const buttonItems = buttonList.map((buttonItem) => {
+            const { title } = buttonItem;
+            const isSelectedButton = buttonItem.title === selectedButton.title;
+
+            return (
+                <MovieToolbarButton
+                    key={`${title}-button`}
+                    id={`${title}-button`}
+                    movieTitle={movieTitle}
+                    buttonText={title}
+
+                    tabIndex={isSelectedButton ? 0 : -1}
+
+                    clickHandler={e => this.handleClick(e, buttonItem)}
+
+                    innerRef={ref => { if (isSelectedButton) this.setSelectedButtonRef(ref); }}
+                />
+            );
+        });
 
         return (
-            <MovieToolbarButton
-                movieTitle={movieTitle}
-                buttonText={title}
-                clickHandler={buttonItem.action}
-            />
+            <div className="btn-group" role="toolbar" aria-label={ariaLabel}>
+                {buttonItems}
+            </div>
         );
-    });
-
-    return (
-        <div className="btn-group" role="toolbar" aria-label={ariaLabel}>
-            {buttonItems}
-        </div>
-    );
-};
+    }
+}
 
 MovieToolbar.propTypes = {
     ariaLabel: PropTypes.string.isRequired,
--- src/primitives/MovieToolbarButton.js
+++ src/primitives/MovieToolbarButton.js
@@ -2,24 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const MovieToolbarButton = ({ movieTitle, buttonText, buttonLabel, clickHandler }) => {
+const MovieToolbarButton = ({
+    movieTitle,
+    buttonText,
+    buttonLabel,
+    clickHandler,
+    tabIndex,
+    innerRef
+}) => {
     const ariaLabel = buttonLabel || `${buttonText} ${movieTitle}`;
 
     return (
-        <button className="btn btn-secondary" aria-label={ariaLabel} onClick={clickHandler}>{buttonText}</button>
+        <button
+            className="btn btn-secondary"
+            aria-label={ariaLabel}
+            onClick={clickHandler}
+            tabIndex={tabIndex}
+            ref={innerRef}
+        >
+            {buttonText}
+        </button>
     );
 };
 
 MovieToolbarButton.defaultProps = {
     buttonText: '',
-    buttonLabel: null
+    buttonLabel: null,
+    tabIndex: 0,
+    innerRef: null
 };
 
 MovieToolbarButton.propTypes = {
     movieTitle: PropTypes.string.isRequired,
     buttonText: PropTypes.string,
     buttonLabel: PropTypes.string,
-    clickHandler: PropTypes.func.isRequired
+    clickHandler: PropTypes.func.isRequired,
+    tabIndex: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    innerRef: PropTypes.func
 };
 
 
