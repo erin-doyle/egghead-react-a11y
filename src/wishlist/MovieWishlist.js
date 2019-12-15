0d0b474be809d2d4cf61947d248cf469500a6495
Erin Doyle <edoyle@cashstar.com>
2019-12-15 09:34:39 -0500

    Improved the label for the link to Add some! even further by providing it an aria-label that is more verbose/descriptive than the link text which works fine being shorted for those who can see it.

--- src/wishlist/MovieWishlist.js
+++ src/wishlist/MovieWishlist.js
@@ -94,7 +94,12 @@ class MovieWishlist extends Component {
                         // No movies yet in the WishList
                         : <div aria-labelledby="noMoviesText addLink" className="no-movies-container">
                             <span id="noMoviesText">
-                                No Movies in your Wish List! <Link id="addLink" to="/browse">Add some Movies!</Link>
+                                No Movies in your Wish List!&nbsp;
+                                <Link id="addLink" to="/browse"
+                                      aria-label="Add some movies to your wishlist now!"
+                                >
+                                    Add some!
+                                </Link>
                             </span>
                         </div>
                     }
